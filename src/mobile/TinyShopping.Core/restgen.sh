c4d2093a7e4b75ddc905c730c9d248407401a885
Johan Karlsson <johan.kson@gmail.com>
2018-01-07 20:58:09 +0100

    Added completed tasks to rest api

--- src/mobile/TinyShopping.Core/restgen.sh
+++ src/mobile/TinyShopping.Core/restgen.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-autorest --input-file=http://192.168.119.131:5000/swagger/v1/swagger.json --csharp.namespace=TinyShopping.Core.Net --add-credentials --output-folder=./net --useDateTimeOffset
+autorest --input-file=http://localhost:5000/swagger/v1/swagger.json --csharp.namespace=TinyShopping.Core.Net --add-credentials --output-folder=./net --useDateTimeOffset
